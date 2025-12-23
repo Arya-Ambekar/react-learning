b0d6beb4d758b63be39b3f99a731458eedd38352
Arya Ambekar <aryaambekar17@gmail.com>
2025-12-23 19:40:29 +0530

    added 02-components project - study of components in react

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Hello guys!</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore,
+        reprehenderit?
+      </p>
+    </div>
+  );
+};
+
+export default Card;
